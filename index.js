59ab17b539c06f733f028dbd99209ca1f855640d
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-05-26 17:15:13 +0400

    added dummy feeds and api implemenation

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+let links = [
+	
+{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+},
+
+{
+  id: 'link-1',
+  url: 'www.facebook.com',
+  description: 'Welcome to facebook!'
+},
+
+{
+  id: 'link-2',
+  url: 'www.google.com',
+  description: 'Welcome to google!'
+},
+
+]
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+	feed: () => links,
   }
 }
 
