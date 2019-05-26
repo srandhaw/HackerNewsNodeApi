56d42c5759c1eeaea7e2b891683c5ff5a5969caa
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-05-26 17:24:48 +0400

    added a feed

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 let links = [
-	
+
 {
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 },
 
-{
-  id: 'link-1',
-  url: 'www.facebook.com',
-  description: 'Welcome to facebook!'
-},
-
-{
-  id: 'link-2',
-  url: 'www.google.com',
-  description: 'Welcome to google!'
-},
 
 ]
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
+let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
 	feed: () => links,
-  }
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
